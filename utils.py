0a2b4a0d5725172ac16667c19fde59b289952868
Bartosz-Biesaga <bartosz.biesaga123@gmail.com>
2025-11-29 14:39:36 +0100

    Modified simulated function for more realistic results

--- utils.py
+++ utils.py
@@ -2,23 +2,23 @@ import math
 import random
 
 def periodic_function(time_s, dominant_signal_parameters=None, noise_signal_parameters=None,
-                      normal_noise_parameters=None):
+                      normal_noise_parameters=None, y_shift=370):
     if dominant_signal_parameters is None:
         dominant_signal_parameters = {
-            "amplitude": 5.0,
-            "frequency_Hz": 1.0,
+            "amplitude": 50.0,
+            "frequency_Hz": 0.8,
             "phase_rad": 0.5,
         }
     if noise_signal_parameters is None:
         noise_signal_parameters = {
-            "amplitude": 0.7,
-            "frequency_Hz": 25.0,
+            "amplitude": 1,
+            "frequency_Hz": 20.0,
             "phase_rad": 0.0,
         }
     if normal_noise_parameters is None:
         normal_noise_parameters = {
             "mean": 0.0,
-            "std": 0.15,
+            "std": 2,
         }
     dsp = dominant_signal_parameters
     nsp = noise_signal_parameters
@@ -26,4 +26,4 @@ def periodic_function(time_s, dominant_signal_parameters=None, noise_signal_para
     dominant_signal = dsp["amplitude"] * math.sin(2 * math.pi * dsp["frequency_Hz"] * time_s + dsp["phase_rad"])
     noise_signal = nsp["amplitude"] * math.sin(2 * math.pi * nsp["frequency_Hz"] * time_s + nsp["phase_rad"])
     normal_noise = random.gauss(nnp["mean"], nnp["std"])
-    return dominant_signal + noise_signal + normal_noise
+    return dominant_signal + noise_signal + normal_noise + y_shift
