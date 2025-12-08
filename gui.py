167c4027d08074c4c6fc94a545df2c9d7b6dab7d
Bartosz-Biesaga <bartosz.biesaga123@gmail.com>
2025-12-08 12:36:33 +0100

    Removed data windowing and amplitude logscale, readed threading

--- gui.py
+++ gui.py
@@ -20,7 +20,7 @@ class ECGInterface:
         self.setup_plot()
 
         self.anim = animation.FuncAnimation(
-            self.fig, self.update_plot, interval=1000./24., cache_frame_data=False
+            self.fig, self.update_plot, interval=1000./30., cache_frame_data=False
         )
 
     def create_widgets(self) -> None:
@@ -60,7 +60,6 @@ class ECGInterface:
             origin='lower',
             aspect='auto',
             interpolation='lanczos',
-            norm=plt.matplotlib.colors.LogNorm(vmin=1, vmax=100)
         )
         self.fig.colorbar(self.spectrogram, ax=self.axes[1])
         self.axes[1].set_title("Spektrogram")
@@ -91,38 +90,16 @@ class ECGInterface:
 
     def update_plot(self, frame) -> tuple[Line2D]:
         try:
-            if self.is_reading:
-                self.bpm_reader.read_data_sample()
-            
             if not self.bpm_reader.time_s:
                 return self.line_raw, self.spectrogram
 
-            current_time = self.bpm_reader.time_s[-1]
-            time_window = 10.0  # seconds
-            start_time = max(0, current_time - time_window)
-            
-            times = self.bpm_reader.time_s
-            start_idx = 0
-            if times[0] < start_time:
-                left, right = 0, len(times) - 1
-                while left < right:
-                    mid = (left + right) // 2
-                    if times[mid] < start_time:
-                        left = mid + 1
-                    else:
-                        right = mid
-                start_idx = left
-            
-            display_time = self.bpm_reader.time_s[start_idx:]
-            display_data = self.bpm_reader.ecg_data[start_idx:]
-            
-            self.line_raw.set_xdata(display_time)
-            self.line_raw.set_ydata(display_data)
+            self.line_raw.set_xdata(self.bpm_reader.time_s)
+            self.line_raw.set_ydata(self.bpm_reader.ecg_data)
 
-            self.axes[0].set_xlim(start_time, current_time + 0.1)
+            self.axes[0].set_xlim(self.bpm_reader.time_s[0], self.bpm_reader.time_s[-1] + 0.1)
 
-            if len(display_data) > 0:
-                ymin, ymax = min(display_data), max(display_data)
+            if len(self.bpm_reader.ecg_data) > 0:
+                ymin, ymax = min(self.bpm_reader.ecg_data), max(self.bpm_reader.ecg_data)
                 margin = (ymax - ymin) * 0.1 if ymax != ymin else 1.0
                 self.axes[0].set_ylim(ymin - margin, ymax + margin)
 
@@ -135,9 +112,8 @@ class ECGInterface:
                 for i, bpm_fft_tuples in enumerate(self.bpm_reader.bpm_fft_tuples_tab):
                     self.bpm_fft_amplitudes[:, i] = [fft_tuple[0] for fft_tuple in bpm_fft_tuples]
                 self.spectrogram.set_data(self.bpm_fft_amplitudes)
-                amp_min = max(self.bpm_fft_amplitudes.min(), 1)
-                amp_max = max(self.bpm_fft_amplitudes.max(), amp_min + 1)
-                self.spectrogram.set_norm(plt.matplotlib.colors.LogNorm(vmin=amp_min, vmax=amp_max))
+                self.spectrogram.set_clim(vmin=self.bpm_fft_amplitudes.min(),
+                                          vmax=self.bpm_fft_amplitudes.max())
                 frequencies = [fft_tuple[1] for fft_tuple in self.bpm_reader.bpm_fft_tuples_tab[0]]
                 extent = [self.bpm_reader.bpm_fft_calculation_time_s[0], self.bpm_reader.bpm_fft_calculation_time_s[-1] + 0.1, frequencies[0], frequencies[-1]]
                 self.spectrogram.set_extent(extent)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from gui import ECGInterface
 from BPMReader import BPMReader
 from utils import SimulatedECGDevice
+import threading
 
 SERIAL_PORT = "COM10"
 BAUDRATE = 9600
@@ -14,7 +15,11 @@ def main():
     if bpm_reader.serial_device is None:
         simulated_device = SimulatedECGDevice(heart_rate_bpm=70)
         bpm_reader.serial_device = simulated_device
-    
+    keep_reading = True
+    def bpm_keep_reading():
+        while keep_reading:
+            bpm_reader.read_data_sample()
+    bpm_reading_thread = threading.Thread(target=bpm_keep_reading)
     app = ECGInterface(root, bpm_reader)
 
     def close_func():
@@ -22,7 +27,10 @@ def main():
         root.destroy()
 
     root.protocol("WM_DELETE_WINDOW", close_func)
+    bpm_reading_thread.start()
     root.mainloop()
+    keep_reading = False
+    bpm_reading_thread.join()
 
 if __name__ == "__main__":
     main()
